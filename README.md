4946c0f6ce09f78e20f8fe29cff5ffa6e2f8553d
Ethan Pike <mooxtime8@gmail.com>
2023-12-09 11:45:50 -0500

    hi

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Morse-Translator
+This is a morse code website supported on Mobile, and PC, and Mac. It can teach you a lot of stuff. Most of this data is from Wikipedia pages.
+https://en.wikipedia.org/wiki/Morse_code
+https://en.wikipedia.org/wiki/Prosigns_for_Morse_code
+Thank you for reading this.
--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 function playDit() {
-	let ding = new Audio('morse.wav');
+	let ding = new Audio('ditsound.wav');
 	ding.play();
 }
 function playDah() {
-	let ding = new Audio('morse (1).wav');
+	let ding = new Audio('dahsound.wav');
 	ding.play();
+}
+
+const input = document.querySelector("input");
+const log = document.getElementById("log");
+
+
+
+addEventListener("keydown",playPeriod)
+function playPeriod(e) {
+	if (e.code== "Period"){playDit()}
+
+}
+
+
+
+
+addEventListener("keydown",playDash)
+function playDash(e) {
+	if (e.code== "Minus"){playDah()}
+
 }
